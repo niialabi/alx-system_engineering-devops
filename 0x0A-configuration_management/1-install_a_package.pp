25e990316a4cdcdbe21fde9f1124f35a0e3134f9
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-11-25 01:44:22 +0000

    werkzeug add

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,9 @@
-# install package
+# install package from pip3
+package { 'werkzueg':
+  ensure    => '2.1.1',
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
