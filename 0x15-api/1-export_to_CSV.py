a4eb01627a118a59237f29a859b9ff32a3444eb7
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2024-01-04 02:22:39 +0000

    export to scv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -20,23 +20,9 @@ if __name__ == "__main__":
     selected_sets = [s for s in todo_data if s['userId'] == user_id]
     done = [d for d in selected_sets if d['completed'] is True]
 
-
     with open(f'{user_id}.csv', 'w', newline='') as csvf:
         csvwriter = csv.writer(csvf, quoting=csv.QUOTE_ALL)
         for selected in selected_sets:
-            csvwriter.writerow([user_id, user_data['username'], selected['completed'], selected['title']])
-
-    """
-    done_count = done.__len__()
-    todo_count = selected_sets.__len__()
-    mid_s = "is done with tasks"
-    s = f"Employee {user_data['name']} {mid_s}({done_count}/{todo_count}): "
-    print(s)
-    for selected in done:
-        print("\t {}".format(selected['title']))
-
-    ----------
-    Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-    File name must be: USER_ID.csv    
-    "2","Antonette","False","suscipit repellat esse quibusdam voluptatem incidunt"
-    """
+            csvwriter.writerow(
+                [user_id, user_data['username'],
+                    selected['completed'], selected['title']])
